e8057411f34ed993f0d13065be21cc43fb45bee9
askeip <paparazzi730@gmail.com>
2016-05-12 12:43:49 +0500

    sqrt added

--- Eval/EvalProgram.cs
+++ Eval/EvalProgram.cs
@@ -9,7 +9,7 @@ namespace EvalTask
 			string input = Console.In.ReadToEnd();
 
             var calc = new Sprache.Calc.XtensibleCalculator();
-
+            calc.RegisterFunction("sqrt", (a) => Math.Sqrt(a));
             // using expressions
             var expr = calc.ParseExpression(input);
             var func = expr.Compile();
