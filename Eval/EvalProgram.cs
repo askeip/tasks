de66e8eabb0faf56894b8cc42e403be776248436
askeip <paparazzi730@gmail.com>
2016-05-12 11:56:30 +0500

    Xcalc added

--- Eval/EvalProgram.cs
+++ Eval/EvalProgram.cs
@@ -7,8 +7,13 @@ namespace EvalTask
 		static void Main(string[] args)
 		{
 			string input = Console.In.ReadToEnd();
-			string output = "TODO";
-			Console.WriteLine(output);
-		}
-	}
+
+            var calc = new Sprache.Calc.XtensibleCalculator();
+
+            // using expressions
+            var expr = calc.ParseExpression(input, x => 2, y => System.Math.PI);
+            var func = expr.Compile();
+            Console.WriteLine(func());
+        }
+    }
 }
--- Eval/EvalProgram_Should.cs
+++ Eval/EvalProgram_Should.cs
@@ -20,6 +20,7 @@ namespace EvalTask
 
         [TestCase("2.2 + 4", Result = 6.2)]
         [TestCase("0 + 0", Result = 0)]
+        [TestCase("0 + 2", Result = 2)]
         [TestCase("-1.5 + -4.8", Result = -5.13)]
         [TestCase("-6.13 + 12.1", Result = 5.88)]
         public void Sum_TwoNums(string expression)
@@ -30,8 +31,28 @@ namespace EvalTask
         [TestCase("2 - 2", Result = 0)]
         [TestCase("2 - (-2.2)", Result = 4.2)]
         [TestCase("0 - 0", Result = 0)]
-        [TestCase("-6.3 - 2", Result = -8.3)]
+        [TestCase("0 - 2", Result = -2)]
+        [TestCase("-6.3 - 2" , Result = -8.3)]
         public void Deduct_TwoNums(string expression)
+        {
+            evalProgram.SolveExpression(expression);
+        }
+
+        [TestCase("2 * 2", Result = 4)]
+        [TestCase("(-2) * (-2.2)", Result = 4.4)]
+        [TestCase("0 * 0", Result = 0)]
+        [TestCase("0 * 2", Result = 0)]
+        [TestCase("-6.3 * 2", Result = -12.6)]
+        public void Multiply_TwoNums(string expression)
+        {
+            evalProgram.SolveExpression(expression);
+        }
+
+        [TestCase("2 / 2", Result = 1)]
+        [TestCase("(-2.2) / (-2)", Result = 1.1)]
+        [TestCase("0 / 2", Result = 0)]
+        [TestCase("-6.4 / 2", Result = -3.2)]
+        public void Divide_TwoNums(string expression)
         {
             evalProgram.SolveExpression(expression);
         }
