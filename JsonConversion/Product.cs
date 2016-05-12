b54d66b70e442c0e402e4433505b4ff41e788a5e
AnatoliDubko <anatoli.dubko@gmail.com>
2016-05-12 11:33:46 +0500

    Json Classes

---
+++ JsonConversion/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonConversion
+{
+    public class ProductV3
+    {
+    }
+
+    public class ProductV2
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Count { get; set; }
+    }
+}
