92095f9a62fe51b0a6cc96d204739d3be1e185bf
Baira Oydopova <obairka@gmail.com>
2016-05-12 13:01:03 +0500

    Add new samples, add property names

--- JsonConversion/JsonConverter_Test.cs
+++ JsonConversion/JsonConverter_Test.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Schema;
 using NUnit.Framework;
@@ -9,8 +11,12 @@ namespace JsonConversion
 	public class JsonConverter_Test_Should
 	{
 		private IJsonConverter jsonConverter;
-		private string jsonV2Example;
-		private string jsonV3Example;
+		private string jsonV2Example2;
+		private string jsonV3Example2;
+
+		private string jsonV2Example1;
+		private string jsonV3Example1;
+
 		private string jsonV3Schema;
 
 		[SetUp]
@@ -19,28 +25,49 @@ namespace JsonConversion
 			// TODO:
 			jsonConverter = new JsonConverter();
 			
-			jsonV2Example = File.ReadAllText("Samples/1.v2.json");
-			jsonV3Example = File.ReadAllText("Samples/1.v3.json");
+			jsonV2Example1 = File.ReadAllText("Samples/1.v2.json");
+			jsonV3Example1 = File.ReadAllText("Samples/1.v3.json");
+
+			jsonV2Example2 = File.ReadAllText("Samples/2.v2.json");
+			jsonV3Example2 = File.ReadAllText("Samples/2.v3.json");
+
 			jsonV3Schema = File.ReadAllText("Samples/warehouse-v3-schema.json");
 		}
 
 		[Test]
 		public void ConvertFromOldVersion_WhenDataIsCorrect()
 		{
-			var result = jsonConverter.Convert(jsonV2Example);
-			Assert.AreEqual(jsonV3Example, result);
+			var result = jsonConverter.Convert(jsonV2Example1);
+
+			CompareJsonStrings(jsonV3Example1, result);
+		}
+
+		[Test]
+		public void ConvertFromOldVersion_WhenDataIsCorrect2()
+		{
+			var result = jsonConverter.Convert(jsonV2Example2);
+			CompareJsonStrings(jsonV3Example2, result);
 		}
 
 		[Test]
 		public void ValidDeserialization_WhenConvertFrom()
 		{
-			var convertedResult = jsonConverter.Convert(jsonV2Example);
+			var convertedResult = jsonConverter.Convert(jsonV2Example1);
 			var schema = JSchema.Parse(jsonV3Schema);
 			var jObject = JObject.Parse(convertedResult);
 
 			var valid = jObject.IsValid(schema);
 
 			Assert.True(valid);
+		}
+
+		public bool CompareJsonStrings(string expected, string actual)
+		{
+			var fixedStringOne = Regex.Replace(expected, @"\s+", string.Empty);
+			var fixedStringTwo = Regex.Replace(actual, @"\s+", string.Empty);
+
+			return string.Equals(fixedStringOne, fixedStringTwo,
+										  StringComparison.OrdinalIgnoreCase);
 		}
 	}
 }
--- JsonConversion/Product.cs
+++ JsonConversion/Product.cs
@@ -1,17 +1,31 @@
-﻿namespace JsonConversion
+﻿using Newtonsoft.Json;
+
+namespace JsonConversion
 {
     public class ProductV3  
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-        public int Count { get; set; }
+		[JsonProperty(PropertyName = "id")]
+		public int Id { get; set; }
+
+		[JsonProperty(PropertyName = "name")]
+		public string Name { get; set; }
+
+		[JsonProperty(PropertyName = "price")]
+		public decimal Price { get; set; }
+
+		[JsonProperty(PropertyName = "count")]
+		public int Count { get; set; }
     }
 
     public class ProductV2
-    {
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-        public int Count { get; set; }
+	{
+		[JsonProperty(PropertyName = "name")]
+		public string Name { get; set; }
+
+		[JsonProperty(PropertyName = "price")]
+		public decimal Price { get; set; }
+
+		[JsonProperty(PropertyName = "count")]
+		public int Count { get; set; }
     }
 }
