e72cf072c91a9b14f4e7da28a5e4264e73ca2d67
askeip <paparazzi730@gmail.com>
2016-05-12 11:45:05 +0500

    Simple sum and Deduct two nums test added

---
+++ Eval/EvalProgram_Should.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvalTask
+{
+    [TestFixture]
+    class EvalProgram_Should
+    {
+        private EvalProgram evalProgram;
+
+        [SetUp]
+        public void SetUp()
+        {
+            evalProgram = new EvalProgram();
+        }
+
+        [TestCase("2.2 + 4", Result = 6.2)]
+        [TestCase("0 + 0", Result = 0)]
+        [TestCase("-1.5 + -4.8", Result = -5.13)]
+        [TestCase("-6.13 + 12.1", Result = 5.88)]
+        public void Sum_TwoNums(string expression)
+        {
+            evalProgram.SolveExpression(expression);
+        }
+
+        [TestCase("2 - 2", Result = 0)]
+        [TestCase("2 - (-2.2)", Result = 4.2)]
+        [TestCase("0 - 0", Result = 0)]
+        [TestCase("-6.3 - 2", Result = -8.3)]
+        public void Deduct_TwoNums(string expression)
+        {
+            evalProgram.SolveExpression(expression);
+        }
+    }
+}
