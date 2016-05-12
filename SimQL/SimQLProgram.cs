9f5e7c38945b8b54bfbc01a84074ad87a1e5a77d
ida <ida@skbkontur.ru>
2016-05-12 11:17:59 +0500

    some piece of code

--- SimQL/SimQLProgram.cs
+++ SimQL/SimQLProgram.cs
@@ -7,7 +7,9 @@ namespace SimQLTask
 {
 	class SimQLProgram
 	{
-		static void Main(string[] args)
+	    private static JObject dataFromJson; // объект, над которым выполняются запросы
+
+	    static void Main(string[] args)
 		{
 			string json = Console.In.ReadToEnd();
 			foreach (var result in ExecuteQueries(json))
@@ -17,13 +19,40 @@ namespace SimQLTask
 		public static IEnumerable<string> ExecuteQueries(string json)
 		{
 			var jObject = JObject.Parse(json);
-			var data = (JObject)jObject["data"];
-			var queries = jObject["queries"].ToObject<string[]>();
-			
-			return queries.Select(q => "TODO");
+			dataFromJson = (JObject)jObject["data"];
+			string[] queries = jObject["queries"].ToObject<string[]>();
+			return queries.Select(ProcessQuery);
 		}
 
+	    public static string ProcessQuery(string query)
+	    {
+	        var functionName = query.Substring(0, 3);
+	        string answer = "";
+
+	        var functionParams = query.Substring(query.IndexOf("("), query.IndexOf(")") - query.IndexOf("("));
+
+	        var function = (Functions) Enum.Parse(typeof(Functions), functionName);
+	        switch (function)
+	        {
+	            case Functions.Sum:
+	                break;
+	            case Functions.Min:
+	                break;
+	            case Functions.Max:
+	                break;
+	            default:
+	                throw new ArgumentOutOfRangeException();
+	        }
 
 
+	        return string.Format("{0} = {1}", query, answer);
+	    }
 	}
+
+    public enum Functions
+    {
+        Sum,
+        Min,
+        Max
+    };
 }
